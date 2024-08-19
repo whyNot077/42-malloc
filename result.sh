9c08f8a5c07040be37fca659d1a55ef1f75a9489
whyNot077 <peppertones0123@gmail.com>
2024-08-19 17:11:27 +0900

    res

--- result.sh
+++ result.sh
@@ -1,6 +1,11 @@
 #!/bin/sh 
+
+gcc -o ./tmp/test0 eval/test0.c && /usr/bin/time -l ./tmp/test0 \
+&& gcc -o ./tmp/test1 eval/test1.c && /usr/bin/time -l ./tmp/test1
+
+./linking.sh
 /usr/bin/time -l ./tmp/test0
-/usr/bin/time -l ./tmp/test2
+/usr/bin/time -l ./tmp/test1
 
 # /usr/bin/time -l ./test0_origin
 # /usr/bin/time -l ./test1_origin
