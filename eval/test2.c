4a4614e06ef3e8152984e77a051896bcd42c4afc
whyNot077 <peppertones0123@gmail.com>
2024-08-19 16:48:30 +0900

    eval

--- eval/test2.c
+++ eval/test2.c
@@ -1,7 +1,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-# include "../include/malloc.h"
 
 void    print(char *s)
 {
@@ -26,6 +25,5 @@ int     main(void)
         free(addr); 
         i++; 
     }
-    show_alloc_mem(); 
     return (0);
 }
