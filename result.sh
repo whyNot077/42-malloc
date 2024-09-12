b27c889edc2223268a1ad189c83e5dc323aaf3ef
whyNot077 <peppertones0123@gmail.com>
2024-09-12 19:47:17 +0900

    modify vector init capacity

--- result.sh
+++ result.sh
@@ -3,10 +3,10 @@
 ./linking.sh
 /usr/bin/time -l ./tmp/test0 > 0
 /usr/bin/time -l ./tmp/test1 > 1
-/usr/bin/time -l ./tmp/test2 > 2
-/usr/bin/time -l ./tmp/test3 > 3
-/usr/bin/time -l ./tmp/test4 > 4
-/usr/bin/time -l ./tmp/test5 > 5
+# /usr/bin/time -l ./tmp/test2 > 2
+# /usr/bin/time -l ./tmp/test3 > 3
+# /usr/bin/time -l ./tmp/test4 > 4
+# /usr/bin/time -l ./tmp/test5 > 5
 
 # /usr/bin/time -l ./tmp/test0_origin
 # /usr/bin/time -l ./tmp/test1_origin
